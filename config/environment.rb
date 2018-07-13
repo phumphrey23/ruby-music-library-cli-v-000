093430320d1e269a7d068d1842ac4bdbb0e203bd
Precious Humphrey <phumphrey23@gmail.com>
2018-07-13 01:30:06 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -13,11 +13,11 @@ module Concerns
 
   module Findable
     def find_by_name(name)
-      all.detect{|a| a.name == name}
+      self.all.detect{|a| a.name == name}
     end
 
     def find_or_create_by_name(name)
-      find_by_name(name) || create(name)
+      self.find_by_name(name) || self.create(name)
     end
   end
 end
