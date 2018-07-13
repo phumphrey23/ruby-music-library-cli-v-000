294ca038f8bddb45d6ede66209e63c7ccfb3a171
Precious Humphrey <phumphrey23@gmail.com>
2018-07-13 03:18:55 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -40,7 +40,7 @@ end
   def list_songs
     sorted_songs = Song.all.sort_by {|song|song.name}
     sorted_songs.each do |song|
-      binding.pry
+      #binding.pry
       puts "#{sorted_songs.index(song) + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
