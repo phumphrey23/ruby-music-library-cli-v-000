2345d9f3aa8ccea33809cb1a9d7e9d5604535441
Precious Humphrey <phumphrey23@gmail.com>
2018-07-13 02:42:44 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ module Concerns
 
   module Findable
     def find_by_name(name)
-      self.all.select{|song| song.name == name}
+      all.select{|song| song.name == name}
     end
 
     def find_or_create_by_name(name)
